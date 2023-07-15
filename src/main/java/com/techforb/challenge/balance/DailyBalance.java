9d4774b65006efd04c292c72af29bac762731a9b
LeooZeballos <zeballosleonel3@gmail.com>
2023-07-15 20:36:25 -0300

    daily balance class, for future balances endpoint

---
+++ src/main/java/com/techforb/challenge/balance/DailyBalance.java
@@ -0,0 +1,35 @@
+package com.techforb.challenge.balance;
+
+import com.techforb.challenge.account.Account;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+/**
+ * Represents the daily balance of an account. Starts at zero and is updated with each transaction that occurs on the account on that day.
+ *
+ * @author Leonel Zeballos
+ */
+@Data
+@AllArgsConstructor
+@Builder
+public class DailyBalance {
+
+    /**
+     * The date of the balance.
+     */
+    private LocalDate date;
+
+    /**
+     * The balance of the account on the date.
+     */
+    private double balance;
+
+    /**
+     * The account the balance belongs to.
+     */
+    private Account account;
+
+}
