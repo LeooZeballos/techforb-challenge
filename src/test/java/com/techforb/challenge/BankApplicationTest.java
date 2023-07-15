d06021906c15f0ef3a7ee16deb88c160e0c3772e
LeooZeballos <zeballosleonel3@gmail.com>
2023-07-15 00:11:13 -0300

    testing class rename

--- src/test/java/com/techforb/challenge/BankApplicationTest.java
+++ src/test/java/com/techforb/challenge/BankApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TechforbChallengeApplicationTests {
+class BankApplicationTest {
 
     @Test
     void contextLoads() {
